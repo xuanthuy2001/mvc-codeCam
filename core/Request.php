2c62afa1a8c3e0417bfc3ad30827dc1713bcb95e
thuy <nguyenxt01012001@gmail.com>
2022-12-02 10:21:47 +0700

    step2 comment

--- core/Request.php
+++ core/Request.php
@@ -7,18 +7,23 @@ namespace app\core;
 class Request
 {
   public function getPath(){
-    $path = $_SERVER['REQUEST_URI'] ?? '/';
+    // mục đích phương thức này là lấy ra đường dẫn khởi nguyên  http://localhost/codeCam/MVC/?id=1 =>  /codeCam/MVC/
+    // b1: lấy tên đường dẫn truyền vào
+    // b2: lấy ra độ dài đường dẫn nguyên vẹn  dùng hàm strpos("URL",'KÍ TỰ CUỐI') => độ dài
+    // b3: lấy ra chuỗi đường dẫn nguyên vẹn  dùng hàm substr("URL",vị trí bắt đầu cắt, vị trí cuối ) => /codeCam/MVC/
+    
+    $path = $_SERVER['REQUEST_URI'] ?? '/'; // b1
   
-    $position  = strpos($path,'?');
+    $position  = strpos($path,'?'); // b2
+    // lấy ra độ dài của 
     // Hàm strpos() sẽ tìm kiếm vị trí đầu tiên của kí tự hoặc chuỗi con xuất hiện trong chuỗi nguồn. Hàm trả về số nguyên là vị trí đầu tiên xuất hiện của kí tự hoặc chuỗi con trong chuỗi nguồn, lưu ý là chuỗi sẽ bắt đầu từ vị trí 0, không phải 1.
     // http://localhost/codeCam/MVC/?id=1   sẽ trả về 13 ==  codeCam/MVC/?
     // http://localhost/codeCam/MVC/        sẽ trả về false
-    
-
     if($position === false){
       return $path;
     }
-    return substr($path , 0 , $position); // /codeCam/MVC/
+    
+    return substr($path , 0 , $position); //b3
   } 
   public function getMethod(){
       return strtolower($_SERVER['REQUEST_METHOD']);
--- core/Router.php
+++ core/Router.php
@@ -13,7 +13,8 @@
 
 
     public function get($path , $callback){
-      $this -> routes['get'][$path] = $callback;
+      $this -> routes['get'][$path] = $callback; 
+      // routes['get']['/'] = function ( ){return "hello world";}
     }
 
     public function resolve(){
@@ -21,11 +22,11 @@
 
       $method = $this -> request -> getMethod();
       
-      $callback = $this -> routes[$method][$path] ?? false;
+      $callback = $this -> routes[$method][$path] ?? false; // routes['get']['/']
         if($callback === false){
           echo "not found ";
           exit();
         }
-        echo call_user_func($callback);
+        echo call_user_func($callback);//không hiểu lắm nhứng chắc là lấy nội dung trả về trong funtion
     }
    }
